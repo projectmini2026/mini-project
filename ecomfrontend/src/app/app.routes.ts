ecb82638f6821645b6d06655ce2c8ecc8ceea0ec
sanmaya5 <sanmayashaji5@gmail.com>
2026-03-18 19:11:05 +0530

    commit

--- ecomfrontend/src/app/app.routes.ts
+++ ecomfrontend/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { FacultyComponent } from './component/faculty-component/faculty-componen
 import { ModuleComponent } from './component/module-component/module-component';
 import { EcComponent } from'./component/ec-component/ec-component';
 import { NfComponent } from './component/nf-component/nf-component';
-import { McComponent } from './component/mc-component/mc-component';
+ 
 
 export const routes: Routes = [
     {path: "",component:LoginComponent},
@@ -15,10 +15,5 @@ export const routes: Routes = [
     {path:"faculty",component:FacultyComponent},
  {path:"module",component:ModuleComponent},
  {path:"ec",component:EcComponent},
-<<<<<<< HEAD
- 
-=======
- {path:"nf",component:NfComponent},
- {path:"mc",component:McComponent}
->>>>>>> 84906d6a507b81fec5ed89e567adcfca321441d3
+ {path:"nf",component:NfComponent}
 ];
